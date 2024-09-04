4554a11693549399d1ee352e34713004cb893ceb
Patrick Gildersleve <22916176+pgilders@users.noreply.github.com>
2024-09-04 14:34:46 +0100

    refactor links to handle errors/warnings and adaptive batch size

--- src/wikitools/links.py
+++ src/wikitools/links.py
@@ -1,6 +1,8 @@
 from wikitools.api import *
 from wikitools.redirects import *
+from aiohttp import ClientConnectorError
 import mwapi
+import time
 
 
 async def parse_links(data, prop):
@@ -53,7 +55,7 @@ async def parse_links(data, prop):
     else:
         return links
 
-async def get_links(session, mode='out', titles=None, pageids=None, norm_map={}, redirect_map={}, id_map={}, namespaces=[0], resolve_redirects=False):
+async def get_links(session, mode='out', titles=None, pageids=None, norm_map={}, redirect_map={}, id_map={}, namespaces=[0], update_maps=False, batchsize=200):
     """Get links on each article in list.
 
     Args:
@@ -83,32 +85,70 @@ async def get_links(session, mode='out', titles=None, pageids=None, norm_map={},
         else:
             mode = [mode]
     
+    # batchsize_threshold = batchsize/2
     return_dict = {}
-    for m in mode:  
+    for m in mode:
+        print('Getting %s-links' % m) 
         params = {modedict[m]['pg']: modedict[m]['pval'],
                 modedict[m]['limit']: 'max',
-                'redirects':resolve_redirects}
+                'redirects':update_maps}
         if m in ['out', 'in']:
             params[modedict[m]['ns']] = ns
-        query_args_list, key, ix = querylister(titles, pageids, generator=(m in ['out', 'in']), norm_map=norm_map, titles_redirect_map=redirect_map, params=params)
-        data = await iterate_async_query(session, query_args_list,
-                                         function=parse_links, args=[modedict[m]['pval']],
-                                         debug=resolve_redirects&(m in ['out', 'in']))
-        if m in ['out', 'in']:
-            data_keys = [x[key] for x in query_args_list]
-            if resolve_redirects:
-                links = dict(zip(data_keys, [x[0] for x in data]))
-                for x in data:
-                    redirect_map.update(x[1])
-                    norm_map.update(x[2])
-                    id_map.update(x[3])
-                missing = [k for k, v in links.items() if v is None]
-                redirect_map.update({x: None for x in missing})
-                id_map.update({x: -1 for x in missing})
+
+        n = 0
+        links = {}
+        size = len(titles) if titles else len(pageids)
+        while n < size:
+            if titles:
+                b_titles = titles[n:n+batchsize]
+                b_pageids = None
             else:
-                links = dict(zip(data_keys, [x[0] for x in data]))
-        else:
-            links = {k[ix]: val for d in data for k, val in d.items()}
+                b_titles = None
+                b_pageids = pageids[n:n+batchsize]
+            try:
+                query_args_list, key, ix = querylister(b_titles, b_pageids,
+                                                    generator=(m in ['out', 'in']),
+                                                    norm_map=norm_map,
+                                                    titles_redirect_map=redirect_map,
+                                                    params=params)
+                data = await iterate_async_query(session, query_args_list,
+                                                function=parse_links, args=[modedict[m]['pval']],
+                                                debug=update_maps&(m in ['out', 'in']))
+            
+            # handle rate error, this doesn't all run automatically, so need to address further issues
+            # except (ValueError, ConnectionError, ClientConnectorError, ConnectionResetError) as v: ????
+            except Exception as v: # Be more specific here
+                print(v)
+                #split arts in half and try again
+                batchsize = batchsize // 2
+                time.sleep(10)
+                print('%.2f%% complete' % (100*n/size))
+                print('Trying again at n=%d with batchsize=%d' % (n, batchsize))
+                continue
+
+            if m in ['out', 'in']:
+                data_keys = [x[key] for x in query_args_list]
+                b_links = dict(zip(data_keys, [x[0] for x in data]))
+                if update_maps:
+                    for x in data:
+                        redirect_map.update(x[1])
+                        norm_map.update(x[2])
+                        id_map.update(x[3])
+                    missing = [k for k, v in b_links.items() if v is None]
+                    redirect_map.update({x: None for x in missing})
+                    id_map.update({x: -1 for x in missing})
+            else:
+                b_links = {k[ix]: val for d in data for k, val in d.items()}
+
+            links.update(b_links)
+
+            n += batchsize
+            if batchsize <= 100:
+                batchsize = batchsize * 2
+                print('Increasing batchsize to %d' % batchsize)
+            elif batchsize < 200:
+                batchsize = 200
+                print('Increasing batchsize to %d' % batchsize)
 
         return_dict[m] = links
     
@@ -119,7 +159,7 @@ async def get_links(session, mode='out', titles=None, pageids=None, norm_map={},
 
 
 async def pipeline_get_links(project, user_agent, titles=None, pageids=None, id_map=None, norm_map=None, redirect_map=None,
-                             gl_args={}, asynchronous=True, session_args={'formatversion':2}):
+                             gl_args={'update_maps':True}, asynchronous=True, session_args={'formatversion':2}):
 
     return_maps = {'id_map': id_map, 'redirect_map': redirect_map,
                    'norm_map': norm_map}
@@ -138,15 +178,16 @@ async def pipeline_get_links(project, user_agent, titles=None, pageids=None, id_
 
     if asynchronous:
         if titles:
-            await fix_redirects(async_session, titles, return_maps['redirect_map'],
-                                return_maps['norm_map'], return_maps['id_map'])
+            await fix_redirects(async_session, titles, redirect_map=return_maps['redirect_map'],
+                                norm_map=return_maps['norm_map'], id_map=return_maps['id_map'])
         links = await get_links(async_session, titles=titles, pageids=pageids, norm_map=return_maps['norm_map'],
-                        redirect_map=return_maps['redirect_map'], **gl_args)
+                        redirect_map=return_maps['redirect_map'], id_map=return_maps['id_map'], **gl_args)
+        await async_session.session.close()
     else:
         raise ValueError('Only async supported at present.')
     
-    return_dict = {'links': links}
-    if any(return_bools.values()):
+    if any(return_bools.values()): # returns new map objects if they didn't exist before. Pre-existing ones are updated in place.
+        return_dict = {'links': links}
         for k, v in return_bools.items():
             if v:
                 return_dict[k] = return_maps[k]
